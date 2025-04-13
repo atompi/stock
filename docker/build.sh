c1148493982922e850680c43742bd2b38adfc93f
atompi <coder.atompi@gmail.com>
2025-04-13 10:01:08 +0800

    fix: docker with mysql8.4

--- docker/build.sh
+++ docker/build.sh
@@ -4,15 +4,15 @@ rm -rf stock
 rsync -av --progress ../../stock . --exclude .git --exclude .idea --exclude *.md --exclude *.bat --exclude __pycache__ --exclude .gitignore --exclude stock/cron --exclude stock/img --exclude stock/docker --exclude instock/cache --exclude instock/log --exclude instock/test
 rm -rf cron
 cp -r ../../stock/cron .
+rm ta-lib-0.6.4-src.tar.gz
+wget https://github.com/TA-Lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
 
-DOCKER_NAME=mayanghua/instock
-TAG1=$(date "+%Y%m")
-TAG2=latest
+DOCKER_NAME=atompi/stock
+TAG=latest
 
-echo " docker build -f Dockerfile -t ${DOCKER_NAME} ."
-docker build -f Dockerfile -t ${DOCKER_NAME}:${TAG1} -t ${DOCKER_NAME}:${TAG2} .
+echo " docker build -f Dockerfile -t ${DOCKER_NAME}:${TAG} ."
+docker build -f Dockerfile -t ${DOCKER_NAME}:${TAG} .
 echo "#################################################################"
-echo " docker push ${DOCKER_NAME} "
+echo " docker push ${DOCKER_NAME}:${TAG} "
 
-docker push ${DOCKER_NAME}:${TAG1}
-docker push ${DOCKER_NAME}:${TAG2}
+#docker push ${DOCKER_NAME}:${TAG}
