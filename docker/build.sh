c71404107d792c8a60941b73545e3ebcaa9d4f5a
atompi <coder.atompi@gmail.com>
2025-04-18 22:44:28 +0800

    fix: startup without run_job

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,8 @@ rm -rf stock
 rsync -av --progress ../../stock . --exclude .git --exclude .idea --exclude *.md --exclude *.bat --exclude __pycache__ --exclude .gitignore --exclude stock/cron --exclude stock/img --exclude stock/docker --exclude instock/cache --exclude instock/log --exclude instock/test
 rm -rf cron
 cp -r ../../stock/cron .
-rm ta-lib-0.6.4-src.tar.gz
-wget https://github.com/TA-Lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
+# rm ta-lib-0.6.4-src.tar.gz
+# wget https://github.com/TA-Lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
 
 DOCKER_NAME=atompi/stock
 TAG=latest
---
+++ instock/bin/init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sleep 30
+
+/usr/local/bin/python3 /data/InStock/instock/job/init_job.py
